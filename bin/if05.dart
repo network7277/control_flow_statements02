13b71d40a5e7b8c10e3f31414570afd565da9d40
network7277 <husanjurayev7277@gmail.com>
2023-03-01 23:25:36 +0500

    05

--- bin/if05.dart
+++ bin/if05.dart
@@ -7,5 +7,39 @@ Returns:
     int: return answer.
 */
 
+int func( int n ){
+    int x1, x2, x3, x4, temp;
+    x1 = n % 10;
+    n = n ~/ 10;
+
+    x2 = n % 10;
+    n = n ~/ 10;
+
+    x3 = n % 10;
+    n = n ~/ 10;
+    x4 = n;
+    temp = x1;
+
+    int answer = 0;
+
+    if ( x1 < temp ){
+        return answer = temp;
+    }
+    
+    if ( temp > x2 ){
+        return answer = temp;
+    }
+
+    if ( temp > x3 ){
+        return answer = temp;
+    }
+
+    if ( temp > x4 ){
+        return answer = temp;
+    }
+    return answer;
+}
+
 void main() {
+    print(func(2546));
 }
