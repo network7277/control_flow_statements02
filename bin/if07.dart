39390e3551a9a27fcb696723d2e1147271daefe8
network7277 <husanjurayev7277@gmail.com>
2023-03-02 00:23:55 +0500

    07

--- bin/if07.dart
+++ bin/if07.dart
@@ -15,5 +15,39 @@ Returns:
     str: return answer. 
 */
 
+String func( int n ){
+    
+    String answer = "";
+    if ( n == 1 ){
+        return answer = "Monday";
+    }
+
+    if ( n == 2 ){
+        return answer = "Tuesday";
+    }
+
+    if ( n == 3 ){
+        return answer = "Wednesday";
+    }
+
+    if ( n == 4 ){
+        return answer = "Thursday";
+    }
+
+    if ( n == 5 ){
+        return answer = "Friday";
+    }
+
+    if ( n == 6 ){
+        return answer = "Saturday";
+    }
+
+    if ( n == 7 ){
+        return answer = "Sunday";
+    }
+    return answer;
+}
+
 void main() {
+    print(func(1));
 }
