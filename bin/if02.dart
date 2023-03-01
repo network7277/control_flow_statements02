79b0f945cc6c63188026c9eae31f10df5882afe7
network7277 <husanjurayev7277@gmail.com>
2023-03-01 22:36:51 +0500

    02

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,28 @@ Args:
 Returns:
     int: return answer.
 */
+int func( int a, int b, int c ){
+    int answer;
+
+    if ( a < b ){
+        if ( b < c ){
+            return answer = a;
+        }
+        else{
+            return answer = c;
+        }
+    }
+    else{
+        if ( b > c ){
+            return answer = c;
+        }
+        else{
+            return answer = b;
+        }
+    }
+    return answer;
+}
+
 void main() {
+    print(func(-5, -3, -1));
 }
